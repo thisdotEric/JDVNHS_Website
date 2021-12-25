5a727bfcccb720c2c05e8f1edaa18679b2c8c1c3
thisdotEric <siguenzajohneric@gmail.com>
2021-12-25 23:32:49 +0800

    Cleanup

--- src/public/js/student-table.js
+++ src/public/js/student-table.js
@@ -1,11 +1,4 @@
 new gridjs.Grid({
-    columns: ['LRN'],
-    data: [['123456789123']],
+  columns: ['LRN'],
+  data: [['123456789123']],
 }).render(document.getElementById('table'));
-
-fetch('http://localhost:3000/v1/student/123456789145')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data.data);
-        console.log(data.data.user_id);
-    });
